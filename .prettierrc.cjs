8be56594aa04552eea0ca779818534d90fa04003
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-02-07 16:19:59 +0300

    ci: added prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+};
