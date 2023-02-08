85fc180f796d9d12a3809c1eff06b6721ccc1610
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-02-08 15:03:19 +0300

    feat: moved types

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,27 +11,6 @@ export interface CBResponseWithPagination<T = object> extends CBResponse<T> {
 	pagination: Pagination;
 }
 
-interface Localprice {
-	amount: string;
-	currency: string;
-}
-
-export interface Invoice {
-	id: string;
-	resource: string;
-	code: string;
-	status: string;
-	business_name: string;
-	customer_email: string;
-	customer_name: string;
-	memo: string;
-	local_price: Localprice;
-	hosted_url: string;
-	created_at: string;
-	updated_at: string;
-	charge: any;
-}
-
 interface Pagination {
 	id: string;
 	starting_after: string;
