6ae2932b3abe5332f374c3232c43142e9a26716b
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-02-21 19:30:23 +0300

    feat: charge tests added

--- test/charge.test.ts
+++ test/charge.test.ts
@@ -9,8 +9,19 @@ describe("coinbase-commerce charges", () => {
 	const client = new CoinbaseCommerceClient(API_KEY);
 
 	test("list charges", async () => {
-		// const { result, error } = await client.listCheckouts();
-		// expect(error).toBeUndefined();
-		// expect(result).toBeDefined();
+		const { result, error } = await client.listCharges();
+		expect(error).toBeUndefined();
+		expect(result).toBeDefined();
+	});
+
+	test("show charge", async () => {
+		const { result, error } = await client.listCharges();
+		expect(error).toBeUndefined();
+		expect(result).toBeDefined();
+		const { result: showResult, error: showError } = await client.showCharge({
+			charge_code_or_charge_id: result!.data[0]!.id,
+		});
+		expect(showError).toBeUndefined();
+		expect(showResult).toBeDefined();
 	});
 });
