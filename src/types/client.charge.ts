493028a68e39afb3ce9f640a52cd57be385d051c
Re4GD <57919250+Re4GD@users.noreply.github.com>
2023-02-21 19:30:04 +0300

    feat: charge function param types added

---
+++ src/types/client.charge.ts
@@ -0,0 +1,26 @@
+import { CurrencyPrice } from "./common";
+
+export type CreateChargeParams = {
+	name: string;
+	description: string;
+	pricing_type: string;
+	local_price: CurrencyPrice;
+	metadata: {
+		customer_id: string;
+		customer_name: string;
+	};
+	redirect_url: string;
+	cancel_url: string;
+};
+
+export type ShowChargeParams = {
+	charge_code_or_charge_id: string;
+};
+
+export type CancelChargeParams = {
+	charge_code_or_charge_id: string;
+};
+
+export type ResolveChargeParams = {
+	charge_code_or_charge_id: string;
+};
